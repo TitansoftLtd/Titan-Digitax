099b4121c49250b0a504045c8d47a93c4993cd3c
Lucky Tsuma <tsumahmumba@gmail.com>
2025-11-24 10:28:53 +0000

    feat: disable digitax sync for disable companies

--- titan_digitax/titan_digitax/utils/sales_invoice.py
+++ titan_digitax/titan_digitax/utils/sales_invoice.py
@@ -3,4 +3,5 @@ from .sales import send_sales_invoice_to_digitax
 
 def on_submit(doc, method):
     if frappe.db.get_value("Company", doc.company, "country") == "Kenya":
-        send_sales_invoice_to_digitax(doc.name)
+        if frappe.db.get_value("Company", doc.company, "custom_enable_company"):
+            send_sales_invoice_to_digitax(doc.name)
